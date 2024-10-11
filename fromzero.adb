369f45cb75bef91b5981c4663a119933df0b9dbc
123pik1 <s197736@student.pg.edu.pl>
2024-10-11 20:53:52 +0200

    new file:   cmpnew

---
+++ fromzero.adb
@@ -0,0 +1,12 @@
+
+
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
+with Ada.Integer_Text_IO;
+with Ada.Numerics.Discrete_Random;
+
+procedure Simulation is
+
+begin
+   Put_Line("Hello, world!");
+end Simulation;
